cb238e054fe424ce30472da34e2b1ec759e5b6c1
acassaigne <anthony.cassaigne@gmail.com>
2020-03-31 16:52:03 +0200

    mob anthony

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,27 +1,29 @@
 import unittest
 
-def create_list(input_string):
+def _create_list(input_string):
     if input_string == "":
         return []
-    return [int(number) for number in input_string.split(",")]
+    result = [number for number in input_string.split(",")]
+    for 
+    return
+
+def _split_string_by_newline(input_string):
+    return [int(number) for number in input_string.split("\n")]
 
 def calculator(input_string):
-    result = create_list(input_string)
+    result = _create_list(input_string)
     if result == []:
         return "0"
-    return "2"
+    return str(sum(result))
 
 
 class TestStringMethods(unittest.TestCase):
-
-    def test_x(self):
-        self.assertEqual([1,1], create_list("1,1"))
-
-    def test_if_create_list_of_empty_string_should_return_empty_list(self):
-        self.assertEqual([], create_list(""))
 
     def test_calculator_empty_string_should_return_0(self):
         self.assertEqual("0", calculator(""))
 
     def test_calculator_string_with_1_comma_1_return_2(self):
         self.assertEqual("2", calculator("1,1"))
+
+    def test_calculator_string_with_1_comma_2_newline_3_return_6(self):
+        self.assertEqual("6", calculator("1,2\n3"))
