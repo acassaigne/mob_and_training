53ddf1240f9d0aba3941349ff87035c5e203480f
Maxime Hemery <maxime.hemery@adeo.com>
2020-03-31 16:22:00 +0200

    Nous avons bien travaillé dis donc

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,7 +1,12 @@
 import unittest
 
+def create_list(input_string):
+    return input_string.split(",")
 
 class TestStringMethods(unittest.TestCase):
 
     def test_x(self):
-        self.assertEqual("1", "2")
+        self.assertEqual(["1","1"], create_list("1,1"))
+
+    def test_if_create_list_of_empty_string_should_return_empty_list(self):
+        self.assertEqual([], create_list(""))
