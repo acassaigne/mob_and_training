b8993250c9585563071ee813e7297a1f3a113ece
acassaigne <anthony.cassaigne@gmail.com>
2020-04-01 17:17:38 +0200

    mob antho

--- test_anagram.py
+++ test_anagram.py
@@ -5,9 +5,16 @@ class Anagram:
         if len(input_string) == 1:
             return [input_string]
         elif len(input_string) == 2:
-            return [input_string,input_string[-1]+input_string[0]]
+            return [input_string, input_string[-1]+input_string[0]]
         else:
-            on est mal
+            result = []
+            result_temp = self.compute(input_string[0:2])
+            rest_character = input_string[2]
+            for word in result_temp:
+                result.append(rest_character + word)
+                result.append(word + rest_character)
+                result.append(word[0] + rest_character + word[1])
+            return sorted(result)
 
 class TestStringMethods(unittest.TestCase):
 
@@ -24,4 +31,4 @@ class TestStringMethods(unittest.TestCase):
     def test_ouzoief(self):
         anagram = Anagram()
         result = anagram.compute("abc")
-        self.assertEqual(["abc","acb","bac","bca","cab","cba"], result)
+        self.assertEqual(sorted(["abc","acb","bac","bca","cab","cba"]), result)
