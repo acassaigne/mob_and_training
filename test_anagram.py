82ae4ea5d3a99803ebd9711b883fedc77293357e
Maxime Hemery <maxime.hemery@adeo.com>
2020-04-06 09:55:15 +0200

    refactor Max

--- test_anagram.py
+++ test_anagram.py
@@ -1,29 +1,5 @@
 import unittest
 
-class Anagram:
-    def compute(self, input_string):
-        if len(input_string) == 1:
-            return [input_string]
-        elif len(input_string) == 2:
-            return [input_string, self.swap(input_string)]
-        else:
-            result_temp = self.compute(input_string[0:2])
-            rest_character = input_string[2]
-            result = self.fill_new_letter(result_temp,rest_character)
-            return sorted(result)
-
-    def swap(self, input_string):
-        return input_string[-1]+input_string[0]
-
-    def fill_new_letter(self, input_string, letter):
-        result = []
-        for word in input_string:
-            result.append(letter + word)
-            result.append(word + letter)
-            result.append(word[0] + letter + word[1])
-        return result
-
-
 class AnagramRufin:
 
     def compute(self, input_string):
@@ -67,17 +43,18 @@ class WordsGenerators:
 class AnagramMax:
 
     def compute(self, input_string):
-        result = self.generate_sub_tuples("", input_string)
+        result = self.generate_all_combinations("", input_string)
         return self.remove_duplicate_words(result)
 
     # TODO : Rename this function
-    def generate_sub_tuples(self, left, rest_of_word):
+    def generate_all_combinations(self, word_in_construction, rest_of_word):
         if len(rest_of_word) == 1:
-            return [left + rest_of_word]
+            return [word_in_construction + rest_of_word]
         result = []
         for i in range(len(rest_of_word)):
-            other_tuples = self.generate_sub_tuples(left + rest_of_word[i], self.remove_ith_letter(i, rest_of_word))
-            result = result + other_tuples
+            other_combinations = self.generate_all_combinations(word_in_construction + rest_of_word[i],
+                                                                self.remove_ith_letter(i, rest_of_word))
+            result = result + other_combinations
         return result
 
     def remove_ith_letter(self, i, word):
@@ -87,24 +64,6 @@ class AnagramMax:
         return list(set(word_list))
 
 class TestStringMethods(unittest.TestCase):
-
-    @unittest.skip("first")
-    def test_from_string_a_should_return_list_with_a(self):
-        anagram = Anagram()
-        result = anagram.compute("a")
-        self.assertEqual(["a"], result)
-
-    @unittest.skip("wait 2 letters")
-    def test_from_string_ab_should_return_list_with_ab_and_ba(self):
-        anagram = Anagram()
-        result = anagram.compute("ab")
-        self.assertEqual(["ab","ba"], result)
-
-    @unittest.skip("wait 3")
-    def test_from_string_abc_should_return_sorted_all_anagrams(self):
-        anagram = Anagram()
-        result = anagram.compute("abc")
-        self.assertEqual(sorted(["abc","acb","bac","bca","cab","cba"]), result)
 
     def test_rufin_one_letter(self):
         anagram = AnagramRufin()
