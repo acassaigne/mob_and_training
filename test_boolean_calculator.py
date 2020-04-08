1f012e6d019ee033b78b258531d2f154aab48675
acassaigne <anthony.cassaigne@gmail.com>
2020-04-08 17:09:57 +0200

    Antho boolean calculator

--- test_boolean_calculator.py
+++ test_boolean_calculator.py
@@ -3,8 +3,9 @@ import unittest
 def boolean_calculator(boolean_expression):
     if boolean_expression == "TRUE":
         return True
-    if boolean_expression == "NOT FALSE":
-        return True
+    if boolean_expression[0:4] == "NOT ":
+        return not boolean_calculator(boolean_expression[4:])
+
     return False
 
 class TestStringMethods(unittest.TestCase):
@@ -20,3 +21,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_not_false_should_return_true(self):
         self.assertEqual(True, boolean_calculator("NOT FALSE"))
+
+    def test_not_not_true_should_return_true(self):
+        self.assertEqual(True, boolean_calculator("NOT NOT TRUE"))
+
+    def test_not_not_not_false_should_return_true(self):
+        self.assertEqual(True, boolean_calculator("NOT NOT NOT FALSE"))
