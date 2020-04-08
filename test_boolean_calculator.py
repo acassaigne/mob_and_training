268ecfd08f9d08521347a1b06df6efb4e60e85fb
rufin-mariette <rufin.m62@orange.fr>
2020-04-08 17:33:10 +0200

    recursive "true or"

--- test_boolean_calculator.py
+++ test_boolean_calculator.py
@@ -1,18 +1,15 @@
 import unittest
 
 def boolean_calculator(boolean_expression):
+    list_of_words = boolean_expression.split(' ')
     if boolean_expression == "TRUE":
         return True
     if boolean_expression[0:4] == "NOT ":
         return not boolean_calculator(boolean_expression[4:])
     if boolean_expression[5:8] == "AND":
         return True and boolean_calculator(boolean_expression[9:])
-    if boolean_expression == "TRUE OR TRUE":
-        return True
-    if boolean_expression == "TRUE OR FALSE":
-        return True
-    if boolean_expression == "FALSE OR TRUE":
-        return True
+    if boolean_expression[5:7] == "OR":
+        return True or boolean_calculator(boolean_expression[8:])
     return False
 
 class TestStringMethods(unittest.TestCase):
@@ -50,5 +47,7 @@ class TestStringMethods(unittest.TestCase):
     def test_true_or_false_should_return_true(self):
         self.assertEqual(True, boolean_calculator("TRUE OR FALSE"))
 
-    def test_false_or_true_should_return_true(self):
-        self.assertEqual(True, boolean_calculator("FALSE OR TRUE"))
+    def test_true_or_true_or_true_should_return_true(self):
+        self.assertEqual(True, boolean_calculator("TRUE OR TRUE OR TRUE"))
+
+    #TODO: continuer à explorer l'axe and/not
