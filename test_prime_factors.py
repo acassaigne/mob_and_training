75ccc8b55d26e0c36a1044bdcf3aac54f209f46b
rufin-mariette <rufin.m62@orange.fr>
2020-04-08 14:30:57 +0200

    up to 4

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -6,16 +6,25 @@ class CannotFindPrimeFactors(Exception):
 
 
 def prime_factor(number):
+    if number > 3:
+        return [number //2, number //2]
+    if number >= 2:
+        return [number]
     raise CannotFindPrimeFactors
 
 class TestPrimeFactorsShould(unittest.TestCase):
 
-    def test_raise_error_for_zero(self):
+    def test_raise_error_for_0(self):
         with self.assertRaises(CannotFindPrimeFactors):
             prime_factor(0)
 
-    def test_y(self):
-        
+    def test_return_2_for_2(self):
+        self.assertEqual([2], prime_factor(2))
 
+    def test_return_3_for_3(self):
+        self.assertEqual([3], prime_factor(3))
+        
+    def test_return_4_for_4(self):
+        self.assertEqual([2,2], prime_factor(4))
 
 
