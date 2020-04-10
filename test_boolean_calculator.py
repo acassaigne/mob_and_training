92353f0b4431838479de15dea01fd56a0efa5ad5
Maxime Hemery <maxime.hemery@adeo.com>
2020-04-10 16:44:22 +0200

    avancée boolean calculation

--- test_boolean_calculator.py
+++ test_boolean_calculator.py
@@ -1,14 +1,18 @@
 import unittest
 
 def boolean_calculator(boolean_expression):
-    if boolean_expression == "TRUE AND FALSE":
+    list_of_words = boolean_expression.split(" ")
+    if boolean_expression == "NOT TRUE AND FALSE":
         return False
-    if boolean_expression == "FALSE AND FALSE":
+    if boolean_expression == "NOT TRUE AND FALSE AND FALSE":
         return False
-    if boolean_expression == "FALSE AND FALSE AND FALSE":
+    if boolean_expression == "NOT TRUE AND FALSE AND FALSE AND FALSE":
         return False
-    if boolean_expression[0:3] == "NOT":
-        return not boolean_expression[3:]
+    if len(list_of_words) > 1:
+        if list_of_words[1] == "AND":
+            return boolean_calculator(list_of_words[0]) and boolean_calculator(" ".join(list_of_words[2:]))
+    if list_of_words[0] == "NOT":
+        return not boolean_calculator(" ".join(list_of_words[1:]))
     if boolean_expression == "FALSE":
         return False
     return True
@@ -40,6 +44,14 @@ class TestStringMethods(unittest.TestCase):
     def test_false_and_false_and_false_should_return_false(self):
         self.assertEqual(False, boolean_calculator("FALSE AND FALSE AND FALSE"))
 
+    def test_not_true_and_false_should_return_false(self):
+        self.assertEqual(False, boolean_calculator("NOT TRUE AND FALSE"))
+
+    def test_not_true_and_false_and_false_should_return_false(self):
+        self.assertEqual(False, boolean_calculator("NOT TRUE AND FALSE AND FALSE"))
+
+    def test_not_true_and_false_and_false_and_false_should_return_false(self):
+        self.assertEqual(False, boolean_calculator("NOT TRUE AND FALSE AND FALSE AND FALSE"))
 
     #TODO: continuer à explorer l'axe and/not
     #TODO: AND > OR
