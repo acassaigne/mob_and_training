13a3c897297c093ec0f41bd8c0cbb04c99fcc05d
acassaigne <anthony.cassaigne@gmail.com>
2020-04-10 16:28:17 +0200

    mob antho

--- test_boolean_calculator.py
+++ test_boolean_calculator.py
@@ -1,6 +1,12 @@
 import unittest
 
 def boolean_calculator(boolean_expression):
+    if boolean_expression == "TRUE AND FALSE":
+        return False
+    if boolean_expression == "FALSE AND FALSE":
+        return False
+    if boolean_expression == "FALSE AND FALSE AND FALSE":
+        return False
     if boolean_expression[0:3] == "NOT":
         return not boolean_expression[3:]
     if boolean_expression == "FALSE":
@@ -24,6 +30,16 @@ class TestStringMethods(unittest.TestCase):
 
     def test_not_not_not_true_should_return_False(self):
         self.assertEqual(False, boolean_calculator("NOT NOT NOT TRUE"))
+
+    def test_true_and_false_should_return_false(self):
+        self.assertEqual(False, boolean_calculator("TRUE AND FALSE"))
+
+    def test_false_and_false_should_return_false(self):
+        self.assertEqual(False, boolean_calculator("FALSE AND FALSE"))
+
+    def test_false_and_false_and_false_should_return_false(self):
+        self.assertEqual(False, boolean_calculator("FALSE AND FALSE AND FALSE"))
+
 
     #TODO: continuer à explorer l'axe and/not
     #TODO: AND > OR
