3a3ba37c729aa3f4595bcc52a0d91d92fb721333
rufin-mariette <rufin.m62@orange.fr>
2020-04-13 15:15:35 +0200

    size 1

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,8 +1,24 @@
 import unittest
 
-class TestRomanNumber(unittest.TestCase):
+class Board():
+    def __init__(self, size):
+        self.size = size
 
-    def test_x(self):
-        a_board = board(2)
-        winner = board.has_winner()
-        self.assertIsNone(winner)
+    def has_winner(self):
+        return "Nobody"
+
+    def play(self, player, row, column):
+        pass
+
+class TestTicTacToe(unittest.TestCase):
+
+    def test_board_size_0_should_have_no_winner(self):
+        a_board = Board(0)
+        winner = a_board.has_winner()
+        self.assertEqual("Nobody", winner)
+
+    def test_board_size_1_should_have_no_winner(self):
+        a_board = Board(1)
+        a_board.play("X", 0, 0)
+        winner = a_board.has_winner()
+        self.assertEqual("X", winner)
