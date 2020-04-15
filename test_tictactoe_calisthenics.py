38bf4e563218a5258e44322b56aa3626aae6e870
Maxime Hemery <maxime.hemery@adeo.com>
2020-04-15 15:44:59 +0200

    create invalidrowupdate

--- test_tictactoe_calisthenics.py
+++ test_tictactoe_calisthenics.py
@@ -8,6 +8,15 @@ class Row:
     def __eq__(self, other):
         return self.values == other.values
 
+    def update(self, position, character):
+        if position >= len(self.values):
+            raise InvalidUpdateRow
+        self.values[position] = character
+
+
+class InvalidUpdateRow(Exception):
+    pass
+
 
 class TestTicTacToe(unittest.TestCase):
 
@@ -21,7 +30,12 @@ class TestTicTacToe(unittest.TestCase):
         a_empty_row = Row(['.'])
         self.assertEqual(a_empty_row, a_row)
 
+    def test_update_row_with_X_should_equal_created_row(self):
+        a_row = Row(['.', '.'])
+        a_row.update(1, 'X')
+        self.assertEqual(Row(['.', 'X']), a_row)
+
     def test_x(self):
-        a_row = Row(['.','.'])
-        a_empty_row = Row(['.','.'])
-        self.assertEqual(a_empty_row, a_row)
+        a_row = Row(['.'])
+        with self.assertRaises(InvalidUpdateRow):
+            a_row.update(1, 'X')
