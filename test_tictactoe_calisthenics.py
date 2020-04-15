09245dd3936eb9a5cc1d68ac9cd63769defb22b2
rufin-mariette <rufin.m62@orange.fr>
2020-04-15 15:34:20 +0200

    row equality

--- test_tictactoe_calisthenics.py
+++ test_tictactoe_calisthenics.py
@@ -6,12 +6,22 @@ class Row:
         self.values = list
 
     def __eq__(self, other):
-        return False
+        return self.values == other.values
 
 
 class TestTicTacToe(unittest.TestCase):
 
-    def test_x(self):
+    def test_empty_list_should_not_equal_X(self):
         a_row = Row(['X'])
         a_empty_row = Row(['.'])
         self.assertNotEqual(a_empty_row, a_row)
+
+    def test_empty_should_equal_empty(self):
+        a_row = Row(['.'])
+        a_empty_row = Row(['.'])
+        self.assertEqual(a_empty_row, a_row)
+
+    def test_x(self):
+        a_row = Row(['.','.'])
+        a_empty_row = Row(['.','.'])
+        self.assertEqual(a_empty_row, a_row)
