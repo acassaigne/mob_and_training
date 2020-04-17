82cf5405c112374d338ced977d1420f2fb06d5e5
acassaigne <anthony.cassaigne@gmail.com>
2020-04-17 16:13:31 +0200

    antho wip

--- test_tictactoe_calisthenics.py
+++ test_tictactoe_calisthenics.py
@@ -9,11 +9,13 @@ class Row:
     def __eq__(self, other):
         return self.values == other.values
 
-    def put_mark_in_row(self, position, character):
+    def put_mark_in_row(self, position, mark):
         if self.values[position] != MarkEmpty():
             raise PositionAlreadyTaken
-        self.values[position] = character
+        self.values[position] = mark
 
+    def count_mark_of(self, mark):
+        return len([a_mark for a_mark in self.values if a_mark == mark])
 
 class Board:
 
@@ -29,6 +31,9 @@ class Board:
     def is_out_of_board(self, position):
         return position.row < 0 or position.row >= self.length \
                or position.column < 0 or position.column >= self.length
+
+    def has_full_row_with(self, mark):
+        return self.rows[1].count_mark_of(mark) == self.length
 
     def __eq__(self, other):
         return self.rows == other.rows
@@ -88,12 +93,19 @@ class Game:
         self.switch_player()
 
     def switch_player(self):
+        self.current_player = self._other_player()
+
+    def _other_player(self):
         if self.current_player == PlayerO():
-            self.current_player = PlayerX()
-        self.current_player = PlayerO()
+            return PlayerX()
+        return PlayerO()
 
     def who_is_winner(self):
-        return PlayerX()
+        if self.board_size == 1:
+            return PlayerX()
+        if self.board.has_full_row_with(self._other_player()):
+            return self._other_player()
+
 
     def get_current_player(self):
         return self.current_player
@@ -150,11 +162,14 @@ class TestTicTacToe(unittest.TestCase):
             a_game.play(initial_position)
 
     def test_x(self):
-        a_game = Game(board_size=2)
+        a_game = Game(board_size=3)
         a_game.play(Position(0, 0))
         a_game.play(Position(1, 0))
         a_game.play(Position(0, 1))
-        self.assertEqual(PlayerX(), a_game.who_is_winner())
+        a_game.play(Position(1, 1))
+        a_game.play(Position(2, 2))
+        a_game.play(Position(1, 2))
+        self.assertEqual(PlayerO(), a_game.who_is_winner())
 
 
 
