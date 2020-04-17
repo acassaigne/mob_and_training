496064b22584e06d5110a9714645559550840db9
Maxime Hemery <maxime.hemery@adeo.com>
2020-04-17 14:33:45 +0200

    position error raising

--- test_tictactoe_calisthenics.py
+++ test_tictactoe_calisthenics.py
@@ -8,8 +8,8 @@ class Row:
     def __eq__(self, other):
         return self.values == other.values
 
-    def update(self, position, character):
-        if position >= len(self.values):
+    def put_mark_in_row(self, position, character):
+        if  position >= len(self.values) or position < 0:
             raise InvalidUpdateRow
         self.values[position] = character
 
@@ -17,10 +17,11 @@ class Row:
 class Board:
 
     def __init__(self, length):
-        self.rows = length * [Row(length)]
+        self.length = length
+        self.rows = self.length * [Row(self.length)]
 
-    def update(self, position, mark):
-        self.rows[position.row].update(position.column, mark)
+    def put_mark_in_board(self, position, mark):
+        self.rows[position.row].put_mark_in_row(position.column, mark)
 
     def __eq__(self, other):
         return self.rows == other.rows
@@ -36,6 +37,8 @@ class Position:
 class InvalidUpdateRow(Exception):
     pass
 
+class InvalidUpdateBoard(Exception):
+    pass
 
 class Mark:
     def __eq__(self, other):
@@ -56,21 +59,27 @@ class MarkO(Mark):
 
 class TestTicTacToe(unittest.TestCase):
 
-    def test_update_row_with_X_should_equal_created_row(self):
+    def test_update_row_with_X_should_not_be_equal_to_created_row(self):
         a_row = Row(2)
-        a_row.update(1, MarkX())
+        a_row.put_mark_in_row(1, MarkX())
         self.assertNotEqual(Row(2), a_row)
 
     def test_should_raise_if_out_of_bounds(self):
         a_row = Row(1)
         with self.assertRaises(InvalidUpdateRow):
-            a_row.update(1, MarkX())
+            a_row.put_mark_in_row(1, MarkX())
 
-    def test_x(self):
+    def test_update_board_with_X_should_not_be_equal_to_created_board(self):
         a_board = Board(2)
-        a_board.update(Position(1,0), MarkX())
+        a_board.put_mark_in_board(Position(1, 0), MarkX())
         self.assertNotEqual(Board(2), a_board)
 
-    def test_y(self):
-        a_board = Board(2)
-        self.assertEqual(Board(2), a_board)
+    def test_negative_position_in_row_should_raise(self):
+        a_row = Row(1)
+        with self.assertRaises(InvalidUpdateRow):
+            a_row.put_mark_in_row(-1, MarkX())
+
+    def test_x(self):
+        a_row = Board(2)
+        with self.assertRaises(InvalidUpdateRow):
+            a_row.put_mark_in_board(Position(-1, 0), MarkX())
