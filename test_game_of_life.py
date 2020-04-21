d6ec3551654c49230af5cdaaf03914ec0db09165
Maxime Hemery <maxime.hemery@adeo.com>
2020-04-21 15:58:09 +0200

    start of code (1 test)

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,9 +1,34 @@
 import unittest
 
 
+class Cell:
+    pass
+
+
+class Position:
+
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+class Grid:
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        self.rows = []
+        self.columns = []
+
+    def seed(self, position):
+        self.rows.append(Cell())
+
+    def __eq__(self, other):
+        return self.rows == other.rows
+
 class TestGameOfLife(unittest.TestCase):
 
     def test_x(self):
         a_grid = Grid(1, 1)
-        a_grid.seed(?)
-        a_cell = C
+        position = Position(0, 0)
+        a_grid.seed(position)
+        self.assertNotEqual(a_grid, Grid(1, 1))
