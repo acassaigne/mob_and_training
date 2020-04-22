affe4ad7a4b7e736f681dcff97847a2cd72046fd
acassaigne <anthony.cassaigne@gmail.com>
2020-04-22 17:14:14 +0200

    wip antho

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -24,17 +24,16 @@ class Position:
         lower_right_corner = Position(min(max_position.row, self.row + 1), min(max_position.column, self.column + 1))
         return upper_left_corner, lower_right_corner
 
-    def add_position_to(self, list_positions, position):
+    def neighbours_accumulator(self, list_positions, position):
         if self.row == position.row and self.column == position.column:
             return list_positions
-        list_positions.append(position)
-        return list_positions
+        return list_positions + [position]
 
     def generate_horizontal_positions_on_fixed_row(self, row, maximum_position):
         result = []
         upper_left_corner, lower_right_corner = self.generate_corners(maximum_position)
         for column in range(upper_left_corner.column, lower_right_corner.column + 1):
-            result = self.add_position_to(result, Position(row, column))
+            result = self.neighbours_accumulator(result, Position(row, column))
         return result
 
     def generate_positions_around(self, maximum_position):
@@ -47,6 +46,10 @@ class Position:
     def __eq__(self, other):
         return self.row == other.row and self.column == other.column 
 
+    def __str__(self):
+        return f"position row={self.row} column={self.column}"
+
+# TODO: class SetNeighbours class iterable
 
 class InvalidPosition(Exception):
     pass
@@ -107,12 +110,23 @@ class Grid:
             result += self.count_alive_cells_around_vertical(Position(position.row, column))
         return result
 
+
     def raise_if_out_of_bounds(self, position):
         if position.row >= self.number_rows or position.row < 0 or position.column >= self.number_columns\
                 or position.column < 0:
             raise InvalidPosition
 
+    def count_alive_cells_around_2(self, position):
+        self.raise_if_out_of_bounds(position)
+        neighbours = position.generate_positions_around(Position(self.number_rows - 1, self.number_columns - 1))
+        result = 0
+        for neighbour in neighbours:
+            if self.rows[neighbour.row][neighbour.column] == AliveCell():
+                result += 1
+        return result
+
     def count_alive_cells_around(self, position):
+        return self.count_alive_cells_around_2(position)
         self.raise_if_out_of_bounds(position)
         return self.count_alive_cells_around_horizontal(position) + self.count_alive_cells_around_vertical(position) \
             + self.count_alive_cells_around_in_diagonals(position)
@@ -205,3 +219,6 @@ class TestGameOfLife(unittest.TestCase):
         result = p.generate_positions_around(Position(0, 1))
         self.assertEqual([Position(0, 1)], result)
 
+    def test_y(self):
+        p = Position(3, 3)
+        self.assertEqual("position row=3 column=3", str(p))
