186bfb00e10f845f0ed3ed888b0028f11b458dac
acassaigne <anthony.cassaigne@gmail.com>
2020-04-24 15:02:30 +0200

    wip antho

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -106,12 +106,18 @@ class GameOfLife:
     def tick(self):
         try:
             count_alive = self.grid.count_alive_cells_around(Position(1, 1))
+
         except InvalidPosition:
             count_alive = 0
+        try:
+            count_alive_0_0 = self.grid.count_alive_cells_around(Position(0, 0))
+        except InvalidPosition:
+            count_alive_0_0 = 0
+        self.grid.kill_all()
         if count_alive == 3:
             self.grid.seed(Position(1, 1))
-        else:
-            self.grid.kill_all()
+        if count_alive_0_0 == 3:
+            self.grid.seed(Position(0, 0))
 
 class TestGameOfLife(unittest.TestCase):
 
@@ -216,7 +222,7 @@ class TestGameOfLife(unittest.TestCase):
         a_game.tick()
         self.assertTrue(a_grid.is_alive(Position(1, 1)))
 
-    def test_x(self):
+    def test_dead_cell_in_position_0_0_in_2_2_grid_with_3_alive_neighbours_should_be_alive_after_tick(self):
         a_grid = Grid(2, 2)
         a_grid.seed(Position(0, 1))
         a_grid.seed(Position(1, 1))
@@ -224,3 +230,4 @@ class TestGameOfLife(unittest.TestCase):
         a_game = GameOfLife(a_grid)
         a_game.tick()
         self.assertTrue(a_grid.is_alive(Position(0, 0)))
+
